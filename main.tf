867ad1b4d98c626acf78b5ed2a53f0c98f40ddc6
Jonah Hirsch <jonahwh@gmail.com>
2019-05-02 14:50:46 -0700

    Update for terraform-0.12

--- main.tf
+++ main.tf
@@ -28,17 +28,17 @@ data "aws_route53_zone" "hasura" {
 }
 
 resource "aws_route53_record" "hasura_validation" {
-  depends_on = ["aws_acm_certificate.hasura"]
-  name       = "${lookup(aws_acm_certificate.hasura.domain_validation_options[0], "resource_record_name")}"
-  type       = "${lookup(aws_acm_certificate.hasura.domain_validation_options[0], "resource_record_type")}"
-  zone_id    = "${data.aws_route53_zone.hasura.zone_id}"
-  records    = ["${lookup(aws_acm_certificate.hasura.domain_validation_options[0], "resource_record_value")}"]
-  ttl        = 300
+  depends_on = [aws_acm_certificate.hasura]
+  name       = aws_acm_certificate.hasura.domain_validation_options[0]["resource_record_name"]
+  type       = aws_acm_certificate.hasura.domain_validation_options[0]["resource_record_type"]
+  zone_id    = data.aws_route53_zone.hasura.zone_id
+  records = [aws_acm_certificate.hasura.domain_validation_options[0]["resource_record_value"]]
+  ttl     = 300
 }
 
 resource "aws_acm_certificate_validation" "hasura" {
-  certificate_arn         = "${aws_acm_certificate.hasura.arn}"
-  validation_record_fqdns = ["${aws_route53_record.hasura_validation.*.fqdn}"]
+  certificate_arn         = aws_acm_certificate.hasura.arn
+  validation_record_fqdns = aws_route53_record.hasura_validation.*.fqdn
 }
 
 # -----------------------------------------------------------------------------
@@ -46,7 +46,8 @@ resource "aws_acm_certificate_validation" "hasura" {
 # -----------------------------------------------------------------------------
 
 # Fetch AZs in the current region
-data "aws_availability_zones" "available" {}
+data "aws_availability_zones" "available" {
+}
 
 resource "aws_vpc" "hasura" {
   cidr_block = "172.17.0.0/16"
@@ -54,31 +55,31 @@ resource "aws_vpc" "hasura" {
 
 # Create var.az_count private subnets for RDS, each in a different AZ
 resource "aws_subnet" "hasura_rds" {
-  count             = "${var.az_count}"
-  cidr_block        = "${cidrsubnet(aws_vpc.hasura.cidr_block, 8, count.index)}"
-  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
-  vpc_id            = "${aws_vpc.hasura.id}"
+  count             = var.az_count
+  cidr_block        = cidrsubnet(aws_vpc.hasura.cidr_block, 8, count.index)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  vpc_id            = aws_vpc.hasura.id
 }
 
 # Create var.az_count public subnets for Hasura, each in a different AZ
 resource "aws_subnet" "hasura_ecs" {
-  count                   = "${var.az_count}"
-  cidr_block              = "${cidrsubnet(aws_vpc.hasura.cidr_block, 8, var.az_count + count.index)}"
-  availability_zone       = "${data.aws_availability_zones.available.names[count.index]}"
-  vpc_id                  = "${aws_vpc.hasura.id}"
+  count                   = var.az_count
+  cidr_block              = cidrsubnet(aws_vpc.hasura.cidr_block, 8, var.az_count + count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  vpc_id                  = aws_vpc.hasura.id
   map_public_ip_on_launch = true
 }
 
 # IGW for the public subnet
 resource "aws_internet_gateway" "hasura" {
-  vpc_id = "${aws_vpc.hasura.id}"
+  vpc_id = aws_vpc.hasura.id
 }
 
 # Route the public subnet traffic through the IGW
 resource "aws_route" "internet_access" {
-  route_table_id         = "${aws_vpc.hasura.main_route_table_id}"
+  route_table_id         = aws_vpc.hasura.main_route_table_id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.hasura.id}"
+  gateway_id             = aws_internet_gateway.hasura.id
 }
 
 # -----------------------------------------------------------------------------
@@ -89,7 +90,7 @@ resource "aws_route" "internet_access" {
 resource "aws_security_group" "hasura_alb" {
   name        = "hasura-alb"
   description = "Allow access on port 443 only to ALB"
-  vpc_id      = "${aws_vpc.hasura.id}"
+  vpc_id      = aws_vpc.hasura.id
 
   ingress {
     protocol    = "tcp"
@@ -110,13 +111,13 @@ resource "aws_security_group" "hasura_alb" {
 resource "aws_security_group" "hasura_ecs" {
   name        = "hasura-tasks"
   description = "allow inbound access from the ALB only"
-  vpc_id      = "${aws_vpc.hasura.id}"
+  vpc_id      = aws_vpc.hasura.id
 
   ingress {
     protocol        = "tcp"
     from_port       = "8080"
     to_port         = "8080"
-    security_groups = ["${aws_security_group.hasura_alb.id}"]
+    security_groups = [aws_security_group.hasura_alb.id]
   }
 
   egress {
@@ -131,13 +132,13 @@ resource "aws_security_group" "hasura_ecs" {
 resource "aws_security_group" "hasura_rds" {
   name        = "hasura-rds"
   description = "allow inbound access from the hasura tasks only"
-  vpc_id      = "${aws_vpc.hasura.id}"
+  vpc_id      = aws_vpc.hasura.id
 
   ingress {
     protocol        = "tcp"
     from_port       = "5432"
     to_port         = "5432"
-    security_groups = ["${aws_security_group.hasura_ecs.id}"]
+    security_groups = [aws_security_group.hasura_ecs.id]
   }
 
   egress {
@@ -154,24 +155,24 @@ resource "aws_security_group" "hasura_rds" {
 
 resource "aws_db_subnet_group" "hasura" {
   name       = "hasura"
-  subnet_ids = ["${aws_subnet.hasura_rds.*.id}"]
+  subnet_ids = aws_subnet.hasura_rds.*.id
 }
 
 resource "aws_db_instance" "hasura" {
-  name                   = "${var.rds_db_name}"
+  name                   = var.rds_db_name
   identifier             = "hasura"
-  username               = "${var.rds_username}"
-  password               = "${var.rds_password}"
+  username               = var.rds_username
+  password               = var.rds_password
   port                   = "5432"
   engine                 = "postgres"
   engine_version         = "10.5"
-  instance_class         = "${var.rds_instance}"
+  instance_class         = var.rds_instance
   allocated_storage      = "10"
   storage_encrypted      = false
-  vpc_security_group_ids = ["${aws_security_group.hasura_rds.id}"]
-  db_subnet_group_name   = "${aws_db_subnet_group.hasura.name}"
+  vpc_security_group_ids = [aws_security_group.hasura_rds.id]
+  db_subnet_group_name   = aws_db_subnet_group.hasura.name
   parameter_group_name   = "default.postgres10"
-  multi_az               = "${var.multi_az}"
+  multi_az               = var.multi_az
   storage_type           = "gp2"
   publicly_accessible    = false
 
@@ -224,7 +225,7 @@ resource "aws_iam_policy" "hasura_log_publishing" {
   path        = "/"
   description = "Allow publishing to cloudwach"
 
-  policy = "${data.aws_iam_policy_document.hasura_log_publishing.json}"
+  policy = data.aws_iam_policy_document.hasura_log_publishing.json
 }
 
 data "aws_iam_policy_document" "hasura_assume_role_policy" {
@@ -241,12 +242,12 @@ data "aws_iam_policy_document" "hasura_assume_role_policy" {
 resource "aws_iam_role" "hasura_role" {
   name               = "hasura-role"
   path               = "/system/"
-  assume_role_policy = "${data.aws_iam_policy_document.hasura_assume_role_policy.json}"
+  assume_role_policy = data.aws_iam_policy_document.hasura_assume_role_policy.json
 }
 
 resource "aws_iam_role_policy_attachment" "hasura_role_log_publishing" {
-  role       = "${aws_iam_role.hasura_role.name}"
-  policy_arn = "${aws_iam_policy.hasura_log_publishing.arn}"
+  role       = aws_iam_role.hasura_role.name
+  policy_arn = aws_iam_policy.hasura_log_publishing.arn
 }
 
 # -----------------------------------------------------------------------------
@@ -259,7 +260,7 @@ resource "aws_ecs_task_definition" "hasura" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
-  execution_role_arn       = "${aws_iam_role.hasura_role.arn}"
+  execution_role_arn       = aws_iam_role.hasura_role.arn
 
   container_definitions = <<DEFINITION
     [
@@ -311,6 +312,7 @@ resource "aws_ecs_task_definition" "hasura" {
       }
     ]
 DEFINITION
+
 }
 
 # -----------------------------------------------------------------------------
@@ -318,28 +320,28 @@ DEFINITION
 # -----------------------------------------------------------------------------
 
 resource "aws_ecs_service" "hasura" {
-  depends_on      = ["aws_ecs_task_definition.hasura", "aws_cloudwatch_log_group.hasura"]
-  name            = "hasura-service"
-  cluster         = "${aws_ecs_cluster.hasura.id}"
-  task_definition = "${aws_ecs_task_definition.hasura.arn}"
-  desired_count   = "${var.multi_az == true ? "2" : "1"}"
-  launch_type     = "FARGATE"
+  depends_on = [
+    aws_ecs_task_definition.hasura,
+    aws_cloudwatch_log_group.hasura,
+    aws_alb_listener.hasura
+  ]
+  name = "hasura-service"
+  cluster = aws_ecs_cluster.hasura.id
+  task_definition = aws_ecs_task_definition.hasura.arn
+  desired_count = var.multi_az == true ? "2" : "1"
+  launch_type = "FARGATE"
 
   network_configuration {
     assign_public_ip = true
-    security_groups  = ["${aws_security_group.hasura_ecs.id}"]
-    subnets          = ["${aws_subnet.hasura_ecs.*.id}"]
+    security_groups = [aws_security_group.hasura_ecs.id]
+    subnets = aws_subnet.hasura_ecs.*.id
   }
 
   load_balancer {
-    target_group_arn = "${aws_alb_target_group.hasura.id}"
-    container_name   = "hasura"
-    container_port   = "8080"
+    target_group_arn = aws_alb_target_group.hasura.id
+    container_name = "hasura"
+    container_port = "8080"
   }
-
-  depends_on = [
-    "aws_alb_listener.hasura",
-  ]
 }
 
 # -----------------------------------------------------------------------------
@@ -348,39 +350,31 @@ resource "aws_ecs_service" "hasura" {
 
 resource "aws_s3_bucket" "hasura" {
   bucket = "hasura-${var.region}-${var.domain}"
-  acl    = "private"
+  acl = "private"
 }
 
 # -----------------------------------------------------------------------------
 # Add IAM policy to allow the ALB to log to it
 # -----------------------------------------------------------------------------
 
-data "aws_elb_service_account" "main" {}
+data "aws_elb_service_account" "main" {
+}
+
+data "aws_iam_policy_document" "hasura" {
+  statement {
+    actions = ["s3:PutObject"]
+    resources = ["${aws_s3_bucket.hasura.arn}/alb/*"]
+
+    principals {
+      type = "AWS"
+      identifiers = [data.aws_elb_service_account.main.arn]
+    }
+  }
+}
 
 resource "aws_s3_bucket_policy" "hasura" {
-  bucket = "${aws_s3_bucket.hasura.id}"
-
-  policy = <<POLICY
-{
-  "Id": "hasuraALBWrite",
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "hasuraALBWrite",
-      "Action": [
-        "s3:PutObject"
-      ],
-      "Effect": "Allow",
-      "Resource": "${aws_s3_bucket.hasura.arn}/alb/*",
-      "Principal": {
-        "AWS": [
-          "${data.aws_elb_service_account.main.arn}"
-        ]
-      }
-    }
-  ]
-}
-POLICY
+  bucket = aws_s3_bucket.hasura.id
+  policy = data.aws_iam_policy_document.hasura.json
 }
 
 # -----------------------------------------------------------------------------
@@ -388,15 +382,15 @@ POLICY
 # -----------------------------------------------------------------------------
 
 resource "aws_alb" "hasura" {
-  name            = "hasura-alb"
-  subnets         = ["${aws_subnet.hasura_ecs.*.id}"]
-  security_groups = ["${aws_security_group.hasura_alb.id}"]
+name            = "hasura-alb"
+subnets         = aws_subnet.hasura_ecs.*.id
+security_groups = [aws_security_group.hasura_alb.id]
 
-  access_logs {
-    bucket  = "${aws_s3_bucket.hasura.id}"
-    prefix  = "alb"
-    enabled = true
-  }
+access_logs {
+bucket  = aws_s3_bucket.hasura.id
+prefix  = "alb"
+enabled = true
+}
 }
 
 # -----------------------------------------------------------------------------
@@ -404,16 +398,16 @@ resource "aws_alb" "hasura" {
 # -----------------------------------------------------------------------------
 
 resource "aws_alb_target_group" "hasura" {
-  name        = "hasura-alb"
-  port        = 8080
-  protocol    = "HTTP"
-  vpc_id      = "${aws_vpc.hasura.id}"
-  target_type = "ip"
+name        = "hasura-alb"
+port        = 8080
+protocol    = "HTTP"
+vpc_id      = aws_vpc.hasura.id
+target_type = "ip"
 
-  health_check {
-    path    = "/"
-    matcher = "302"
-  }
+health_check {
+path    = "/"
+matcher = "302"
+}
 }
 
 # -----------------------------------------------------------------------------
@@ -421,15 +415,15 @@ resource "aws_alb_target_group" "hasura" {
 # -----------------------------------------------------------------------------
 
 resource "aws_alb_listener" "hasura" {
-  load_balancer_arn = "${aws_alb.hasura.id}"
-  port              = "443"
-  protocol          = "HTTPS"
-  certificate_arn   = "${aws_acm_certificate.hasura.arn}"
+load_balancer_arn = aws_alb.hasura.id
+port              = "443"
+protocol          = "HTTPS"
+certificate_arn   = aws_acm_certificate.hasura.arn
 
-  default_action {
-    target_group_arn = "${aws_alb_target_group.hasura.id}"
-    type             = "forward"
-  }
+default_action {
+target_group_arn = aws_alb_target_group.hasura.id
+type             = "forward"
+}
 }
 
 # -----------------------------------------------------------------------------
@@ -437,13 +431,14 @@ resource "aws_alb_listener" "hasura" {
 # -----------------------------------------------------------------------------
 
 resource "aws_route53_record" "hasura" {
-  zone_id = "${data.aws_route53_zone.hasura.zone_id}"
-  name    = "hasura.${var.domain}"
-  type    = "A"
+zone_id = data.aws_route53_zone.hasura.zone_id
+name    = "hasura.${var.domain}"
+type    = "A"
 
-  alias {
-    name                   = "${aws_alb.hasura.dns_name}"
-    zone_id                = "${aws_alb.hasura.zone_id}"
-    evaluate_target_health = true
-  }
+alias {
+name                   = aws_alb.hasura.dns_name
+zone_id                = aws_alb.hasura.zone_id
+evaluate_target_health = true
 }
+}
+
