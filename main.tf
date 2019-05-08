0b06ceee11ec3e524830d2fae09a59609b95d261
Dave Tapley <dukedave@gmail.com>
2019-05-08 11:16:03 -0700

    terraform fmt

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ data "aws_availability_zones" "available" {
 }
 
 resource "aws_vpc" "hasura" {
-  cidr_block = "172.17.0.0/16"
+  cidr_block           = "172.17.0.0/16"
   enable_dns_hostnames = var.vpc_enable_dns_hostnames
 
   tags = {
