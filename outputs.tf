82e59fd41f913175100094cda0f8563e25a84937
Aaron <aaron@aaron-macbook-pro.local>
2020-04-21 11:39:14 -0700

    #7 Write descriptions for outputs

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,19 @@
 output "vpc" {
+  description = "VPC created to hold Hasura resources"
   value = aws_vpc.hasura
 }
 
 output "private_subnets" {
+  description = "Private subnets created for RDS within the VPC, each in a different AZ"
   value = aws_subnet.hasura_private
 }
 
 output "public_subnets" {
+  description = "Public subnets created for Hasura within the VPC, each in a different AZ"
   value = aws_subnet.hasura_public
 }
 
 output "ecs_security_group" {
+  description = "Security group controlling access to the ECS tasks"
   value = aws_security_group.hasura_ecs
 }
